411e59525f0ccb49e9a73c1e035f364bbdd13089
Danihelguera <danielhelguerasanchez@gmail.com>
2021-12-15 18:45:48 +0100

    final miercoles

--- pages/mapas.py
+++ pages/mapas.py
@@ -1,24 +1,38 @@
 import streamlit as st
-from streamlit_folium import folium_static
+from   streamlit_folium import folium_static
 import streamlit.components.v1 as components
 import codecs
 import folium
 import requests
-from src.geocoding import consigue_lat_long
+from   src.geocoding import consigue_lat_long
+from   src.geocoding import consigue_type_point_lat_long
+from   src.geocoding import type_point
+from   src.geocoding import permutar_lat_x_long
+from   src.geocoding import buscar_barrio
+from   src.geocoding import buscar_codigo_postal
+from   pymongo import MongoClient
 
 
 def app():
-    st.write("""
-    # MAPAS
-    """)
+    st.write("""# MAPAS""")
     
+    default_origin = "Puerta del Sol, Madrid"
+    origin = st.sidebar.text_input("Introduce Origen", default_origin)
+    origin_ll = consigue_lat_long(origin)
+    origin_ll_T = permutar_lat_x_long(consigue_type_point_lat_long(origin_ll))
+    
+    default_destiny = "Paseo de la Chopera, 14 Madrid"
+    destiny = st.sidebar.text_input("Introduce Destino", default_destiny)
+    destiny_ll = consigue_lat_long(destiny)
+    destiny_ll_T = permutar_lat_x_long(consigue_type_point_lat_long(destiny_ll))
+
     size_mapa = st.sidebar.slider("Zoom del mapa :",min_value = 9 , max_value=15, step=1, value = 12)
     tipo_mapa = st.sidebar.radio("Selecciona la forma de busqueda:" , ['OpenStreetMap', 
                                                                        'Stamen Terrain', 
                                                                        'Stamen Toner', 
                                                                        'TomTom', 
                                                                        'Image', 
-                                                                       'WaterColor']) 
+                                                                       'WaterColor'], index = 2) 
     if   tipo_mapa == 'OpenStreetMap' :
         tiles_mapa = 'OpenStreetMap'
         attr_mapa  = ''
@@ -43,20 +57,65 @@ def app():
         tiles_mapa = 'https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg'
         attr_mapa  = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
 
+            
+    search_by = {}
+    search_by["origin"]  = buscar_barrio(origin_ll_T)
+    search_by["destiny"] = buscar_barrio(destiny_ll_T)
+    if (search_by["origin"] != False) and (search_by["destiny"] != False) :
+        search_by["tipo"]    = "barrio"
+    else :
+        search_by["origin"]  = buscar_codigo_postal(origin_ll_T) 
+        search_by["destiny"] = buscar_codigo_postal(destiny_ll_T) 
+        if (search_by["origin"] != False) and (search_by["destiny"] != False) :
+            search_by["tipo"]    = "codigo_postal"
+        else   :         
+            search_by["tipo"]    = "Fuera de rango geografico (Comunidad de Madrid)"
+            search_by["origin"]  = "No encontrado"
+            search_by["destiny"] = "No encontrado"
+
+    if   search_by["tipo"] == "Fuera de rango geografico (Comunidad de Madrid)" :
+        st.write(search_by)
+
+    else:
+        st.write("TIEMPO MEDIO = XXX MINUTOS")
+        client = MongoClient("localhost:27017")
+        db = client.get_database("Coropleticos_Madrid")
+        if search_by["tipo"] == "barrio" :
+            c  = db.get_collection("Barrios")
+        elif search_by["tipo"] == "codigo_postal" :
+            c  = db.get_collection("Codigos_Postales")
+        geo_Reduced = {'type'    : 'FeatureCollection', 'features': [] }
+        query = {"geometry": {"$geoIntersects": {"$geometry": origin_ll_T}}}
+        origin_geo = c.find_one(query)
+        origin_geo.pop('_id')
+        geo_Reduced["features"].append(origin_geo)
+        query = {"geometry": {"$geoIntersects": {"$geometry": destiny_ll_T}}}
+        destiny_geo = c.find_one(query)
+        destiny_geo.pop('_id')
+        geo_Reduced["features"].append(destiny_geo)
         
-    default_value_goes_here = "Puerta del Sol, Madrid"
-    user_input = st.text_input("Introduce dirección", default_value_goes_here)
-    latlon = consigue_lat_long(user_input)
-
-    icono = folium.Icon(color="blue",
-             prefix = "fa",
-             icon="rocket",
-             icon_color="black")
-    datos = {"location": latlon, "tooltip": "Lo que buscas", "icon":icono}
-    mark = folium.Marker(**datos)
-
-
-    map_1 = folium.Map(location = latlon, zoom_start=size_mapa, tiles=tiles_mapa , attr=attr_mapa)
-    mark.add_to(map_1)
-
-    folium_static(map_1)
+        origin_icon = folium.Icon(color="blue",
+                                  prefix = "fa",
+                                  icon="rocket",
+                                  icon_color="black")
+        origin_icon_data  = {"location": origin_ll, "tooltip": "Origen", "icon":origin_icon}
+        origin_mark = folium.Marker(**origin_icon_data)
+        destiny_icon = folium.Icon(color="red",                          
+                                   prefix = "fa",
+                                   icon="rocket",
+                                   icon_color="black")
+        destiny_icon_data = {"location": destiny_ll, "tooltip": "Destino", "icon":destiny_icon}
+        destiny_mark = folium.Marker(**destiny_icon_data)
+        punto_central = [(origin_ll[0]+destiny_ll[0])/2 , (origin_ll[1]+destiny_ll[1])/2 ]
+        map_1 = folium.Map(location = punto_central, zoom_start=size_mapa, tiles=tiles_mapa , attr=attr_mapa)
+        origin_mark.add_to(map_1)
+        destiny_mark.add_to(map_1)
+        
+        folium.GeoJson(geo_Reduced,
+                       style_function=lambda feature: 
+                           {'fillColor': 'blue',
+                            'color': 'blue',
+                            'weight': 1,
+                            'dashArray': '5, 5'}
+                        ).add_to(map_1)
+        folium_static(map_1)
--- src/geocoding.py
+++ src/geocoding.py
@@ -1,9 +1,47 @@
 from geopy.geocoders import Nominatim
+import json
+from pymongo import MongoClient
 
-def consigue_lat_long(user_input):
+client = MongoClient("localhost:27017")
+db = client.get_database("Coropleticos_Madrid")
+
+def type_point(lista):
+    return {"type":"Point", "coordinates": lista}
+
+def consigue_lat_long(direccion):
+    """
+    Esta función saca las coordenadas de la dirección que le pases
+    y la devuelve como lista [lat,long]
+    """
     geolocator = Nominatim(user_agent="dddddddddddddddddd@gmail.com")
-    location = geolocator.geocode(user_input)
+    location = geolocator.geocode(direccion)
     return [location.latitude, location.longitude]
-    
-    
-    
+
+def consigue_type_point_lat_long(direccion):
+    """
+    Esta función saca las coordenadas de la dirección que le pases
+    y la devuelve en type:point 
+    """
+    return type_point( consigue_lat_long(direccion) )
+
+def permutar_lat_x_long(lugar):
+    lugar_T = type_point([lugar["coordinates"][1] , lugar["coordinates"][0] ])
+    return lugar_T
+
+def buscar_barrio(lugar) :
+    query = {"geometry": {"$geoIntersects": {"$geometry": lugar}}}
+    c    = db.get_collection("Barrios")
+    proy = {"_id":0, "properties.DISPLAY_NAME": 1, "properties.MOVEMENT_ID": 1}
+    try:
+        return c.find_one(query,proy)["properties"]['MOVEMENT_ID']
+    except:
+        return False
+
+def buscar_codigo_postal(lugar) :
+    query = {"geometry": {"$geoIntersects": {"$geometry": lugar}}}
+    c    = db.get_collection("Codigos_Postales")
+    proy = {"_id":0, "properties.GEOCODIGO": 1   , "properties.MOVEMENT_ID": 1}
+    try:
+        return c.find_one(query,proy)["properties"]['MOVEMENT_ID']
+    except:
+        return False
